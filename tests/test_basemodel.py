b0b6a078ee735740018c913a782a550f52bb9459
utukj <utukphd@gmail.com>
2022-08-03 14:05:22 +0100

    first test

---
+++ tests/test_basemodel.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Test BaseModel"""
+
+from datetime import datetime
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.bm1 = BaseModel()
+        self.bm2 = BaseModel()
+
+    def test_init(self):
+        self.assertNotEqual(self.bm1.id, self.bm2.id)
+
+    def test_save(self):
+        self.bm1.save()
+        self.assertAlmostEqual(self.bm1.updated_at, datetime.now())
