c965d1cae1a9ef4c0b6f5f5ab8cfe988d5d23d42
utukj <utukphd@gmail.com>
2022-08-06 22:35:47 +0100

    missed place

--- models/place.py
+++ models/place.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-"""State module"""
-from models.base_model import BaseModel
-
-
-class Place(BaseModel):
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
-=======
 #!/usr/bin/python3
 """State module"""
 from models.base_model import BaseModel
@@ -35,4 +16,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
->>>>>>> f2f06b18c3c9734cb473ccd4bfc353c72b2619ef
