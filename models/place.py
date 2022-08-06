f2f06b18c3c9734cb473ccd4bfc353c72b2619ef
ThisWhiney <uziwinnie@gmail.com>
2022-08-06 13:16:59 -0700

    add documentation

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
+    """place model"""
     city_id = ""
     user_id = ""
     name = ""
--- models/review.py
+++ models/review.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """review model"""
     place_id = ""
     user_id = ""
     text = ""
